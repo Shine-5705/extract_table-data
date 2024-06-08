1a3c747e5bf761cefa83e59e86292256bdc3bdf9
Shine-5705 <guptashine5002@gmail.com>
2024-06-08 21:41:06 +0530

    added files

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.font_manager as fm
 
-# Initialize PaddleOCR with the appropriate model
 ocr = PaddleOCR(use_angle_cls=True, lang='en')
 
 # Directory containing the images
